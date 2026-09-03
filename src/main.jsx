d6dec665e13b24ab3b4114a08f18b2f9ded21602
savivah-lab <kelvoonlineproceeds@gmail.com>
2026-09-03 11:50:13 +0300

    Wrap SavivahApp in ErrorBoundary component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SavivahApp from "./App.jsx";
+import { ErrorBoundary } from "./ErrorBoundary.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SavivahApp />
+    <ErrorBoundary>
+      <SavivahApp />
+    </ErrorBoundary>
   </React.StrictMode>
 );
